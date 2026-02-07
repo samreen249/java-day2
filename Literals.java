d8e6513d5163f5de5c33a59c699803f76ff97404
samreen249 <shaiksamreen1119@gmail.com>
2026-02-07 10:59:30 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,13 @@
+public class Literals {
+    public static void main(String[] args)
+    {
+    int a=10;
+    int b=012;
+    int c= 0x1B;
+    int d=0b11;
+    float f=2.3f;
+    char e='s';
+    boolean g=true;
+    System.out.println(a);
+    }
+}
