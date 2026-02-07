fe0536ee36a322cb76a0953a0f4507a03a8db97c
samreen249 <shaiksamreen1119@gmail.com>
2026-02-07 14:55:41 +0530

    Add files via upload

---
+++ OperatorTask.java
@@ -0,0 +1,21 @@
+public class OperatorTask {
+    public static void main(String args[]){
+      int a=40;
+      int b=-20; 
+      System.out.println(a+b); 
+      System.out.println(+a); 
+      if(a==b){
+        System.out.println("negative number");
+      }
+      else if(a>0&&a%2==0){
+    System.out.println("EVEN NUMBER");
+      }
+        else{
+            System.out.println("positive number");
+        
+      } 
+
+
+    }
+    
+}
