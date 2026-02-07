37f01354a02356e7345bbfc5f0b804ff0a3d51f8
samreen249 <shaiksamreen1119@gmail.com>
2026-02-07 11:11:12 +0530

    Add files via upload

---
+++ TotalAverage.java
@@ -0,0 +1,12 @@
+public class TotalAverage {
+    public static void main(String args[]){
+        int maths=95;
+        int physics=97;
+        int chemisry=86;
+        int total=maths+physics+chemisry;
+        int average=total/2;
+        System.out.println(total);
+        System.out.println(average);
+
+    }
+}
