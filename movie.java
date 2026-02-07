b1759d0e43dfd22201e5c2cb53966ad59af3b45b
samreen249 <shaiksamreen1119@gmail.com>
2026-02-07 11:33:50 +0530

    Add files via upload

---
+++ movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    public static void main(String[] args) {
+        String name="bhadra";
+        int ticket=123;
+        System.out.println(name);
+        System.out.println(ticket);
+    }
+}
