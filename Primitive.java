1759eeff1a7af8383e01766a196b272fef9cb5d5
samreen249 <shaiksamreen1119@gmail.com>
2026-02-07 10:55:59 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,25 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int age=25;
+        String name="samreen";
+        char grade='s';
+        int marks=99;
+        boolean status=true;
+        byte a=123;
+        short b=150;
+        float c=3.14f;
+        double d=3.13d; 
+        long e=106758l;
+        System.out.println(age);
+        System.err.println(name);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(status);
+        System.out.println(a);
+        System.err.println(b);
+        System.err.println(c);
+        System.err.println(d);
+        System.err.println(e);
+    }
+    
+}
